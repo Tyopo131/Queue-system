676546b61937fde44ffda230e58c793829cfb765
Tyopo131 <samecob62@gmail.com>
2024-04-29 09:59:40 +0100

    Add std::cin.get()

--- Queue system/Queue main.cpp
+++ Queue system/Queue main.cpp
@@ -1,90 +1,4 @@
-#include <iostream>
-
-
-
-namespace Qsys {
-	
-	template<typename Qtype> class Queue {
-		Qtype*** Qptr = &queue;
-		Qtype** queue = nullptr;
-		int maxSize;
-
-	public:
-		Qtype* operator[](int index){
-			_ASSERT((index >= 0) && (index < maxSize));
-			return *(queue + index);
-		}
-		
-	
-		Queue(int maxSize) {
-			queue = new Qtype* [maxSize];
-			_ASSERT(queue != nullptr);
-			
-			this->maxSize = maxSize;
-
-		}
-		int set(int index, Qtype val) {
-			if (*(queue + index) != nullptr) {
-				**(queue + index) = val;
-				return 0;
-			}
-			return 1;
-		}
-		void MoveToFront() {
-			for (int timer = 0; timer < maxSize; timer++) {
-				if ((*(queue + timer) != nullptr)) {
-					if (*(queue + (timer - 1)) == nullptr) {
-						for (int timer2 = timer; (*(queue + (timer2 - 1)) == nullptr) && !(queue + (timer2 - 1) < queue); timer2--) {
-							*(queue + (timer2 - 1)) = *(queue + timer2);
-							*(queue + timer2) = nullptr;
-						}
-					}
-				}
-			}
-		}
-		void push() {
-			delete *queue;
-			*queue = nullptr;
-			MoveToFront();
-			
-		}
-
-		
-		enum PUT_RESULT {
-			SUCCESS = 0, SLOT_FULL = 2, FAIL = 1
-		};
-
-
-		PUT_RESULT put(Qtype* heapAllocPtr) {
-			if (*(queue + (maxSize - 1)) != nullptr) return SLOT_FULL;
-			*(queue + (maxSize - 1)) = heapAllocPtr;
-			
-			return SUCCESS;
-		}
-		void fillQueue() {
-			for (int timer = 0; timer < maxSize; timer++) {
-				*(queue + timer) = nullptr;
-			}
-
-		}
-
-		
-		~Queue() {
-			for (int timer = 0; timer < maxSize; timer++) delete *(queue + timer);
-			delete[] queue;
-		}
-	};
-	
-
-	
-	
-
-}
-
-
-
-
-
+#include "Qsys.h"
 
 using namespace Qsys;
 
@@ -100,5 +14,6 @@ int main() {
 	queue.set(0, 23); 
 	queue.push();
 	std::cout << *queue[0];
+	std::cin.get();
 }
 
